8306f9d41a29db5b41359b47f802301b3fc4d11a
Gage Larsen <glarsen@aquaveo.com>
2018-05-10 12:56:28 -0600

    remove gcc specific

--- recipes/xmscore/build.sh
+++ recipes/xmscore/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-CC=${PREFIX}/bin/gcc
-CXX=${PREFIX}/bin/g++
-
 mkdir -p build
 cd build
 ${BUILD_PREFIX}/bin/cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=True -DIS_CONDA_BUILD=True -DBOOST_ROOT=${BUILD_PREFIX} ..
