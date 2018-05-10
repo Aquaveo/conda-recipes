aec8ad209b170904cdfd7a9464f8a685c081a7a3
Gage Larsen <37311335+gagelarsen@users.noreply.github.com>
2018-05-10 09:42:30 -0600

    print statement for make

--- recipes/xmscore/build.sh
+++ recipes/xmscore/build.sh
@@ -2,5 +2,6 @@
 mkdir -p build
 cd build
 ${BUILD_PREFIX}/bin/cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=True -DIS_CONDA_BUILD=True -DBOOST_ROOT=${BUILD_PREFIX} ..
+which make
 make -j${CPU_COUNT}
 make install
