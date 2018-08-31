407e15d8dcf533d23f8eef2eb2846bc7d0821dad
Chandler Scott <cscott@aquaveo.com>
2018-08-31 16:51:59 -0600

    Add warning message if OPENSSL_CONF is already set on activate

--- scripts/activate.bat
+++ scripts/activate.bat
@@ -1,3 +1,5 @@
 if not defined OPENSSL_CONF (
   set OPENSSL_CONF="%CONDA_PREFIX%/share/cacurl/cacurl-openssl.cnf"
+) else (
+  echo "WARNING: OPENSSL_CONF was already set, so I am not touching it. CACurl may not work as you expect..."
 )
--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 if [[ -z "$OPENSSL_CONF" ]]; then
   export OPENSSL_CONF="${CONDA_PREFIX}/share/cacurl/cacurl-openssl.cnf"
+else
+  print "WARNING: OPENSSL_CONF was already set, so I am not touching it. CACurl may not work as you expect..."
 fi
