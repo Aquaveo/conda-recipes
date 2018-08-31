8ef616cfa90d0465d4e977453914e83a70c69e34
Chandler Scott <cscott@aquaveo.com>
2018-08-31 16:49:59 -0600

    Fix shared library extention mismatch in openssl config

--- build.sh
+++ build.sh
@@ -7,4 +7,9 @@ mkdir -p $SHARE_DIR
 
 cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/cacurl-activate.sh
 cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/cacurl-deactivate.sh
-cp $RECIPE_DIR/conf/openssl.cnf $SHARE_DIR/cacurl-openssl.cnf
+# If OSX_ARCH is undefined, assume we are on linux
+if [ -z "$OSX_ARCH" ]; then 
+    cp $RECIPE_DIR/conf/openssl.linux.cnf $SHARE_DIR/cacurl-openssl.cnf
+else
+    cp $RECIPE_DIR/conf/openssl.osx.cnf $SHARE_DIR/cacurl-openssl.cnf
+fi
