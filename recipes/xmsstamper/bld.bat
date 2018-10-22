604e23df7221901eb9811cbd820196aa027e9584
Andrew Clark <aclark@aquaveo.local>
2018-10-22 09:20:05 -0600

    Add stamper files

---
+++ recipes/xmsstamper/bld.bat
@@ -0,0 +1,10 @@
+if not exist "build\" mkdir build
+cd build
+
+%BUILD_PREFIX%\Library\bin\cmake.exe -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release -DIS_CONDA_BUILD=True -DIS_PYTHON_BUILD=True -DCONDA_PREFIX=%CONDA_PREFIX% -DPYTHON_SITE_PACKAGES=%SP_DIR% -DBoost_NO_SYSTEM_PATHS=True -DBOOST_ROOT=%BUILD_PREFIX%\Library ..
+
+nmake -f Makefile
+nmake install -f Makefile
+
+if not exist "%SP_DIR%" mkdir "%SP_DIR%"
+COPY *.pyd %SP_DIR%
--- recipes/xmsstamper/build.sh
+++ recipes/xmsstamper/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 mkdir -p build
 cd build
+
 ${BUILD_PREFIX}/bin/cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DIS_CONDA_BUILD=True -DIS_PYTHON_BUILD=True -DCONDA_PREFIX=${BUILD_PREFIX} -DPYTHON_SITE_PACKAGES=${SP_DIR} -DBoost_NO_SYSTEM_PATHS=True -DBOOST_ROOT=${BUILD_PREFIX} ..
+
 make -j${CPU_COUNT}
 make install
+
+mkdir -p "${SP_DIR}"
+echo "--------------------------------------"
+ls -lah ${SP_DIR}
+echo "--------------------------------------"
+ls -lah
+echo "--------------------------------------"
+cp *.so ${SP_DIR}
